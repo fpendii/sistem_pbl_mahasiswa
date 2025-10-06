2a3618bbb7c44cfb9daf5481e59ae6937030b91a
fpendii <144331827+fpendii@users.noreply.github.com>
2025-10-06 21:00:34 +0800

    update

--- app/Http/Controllers/ControllerUser.php
+++ app/Http/Controllers/ControllerUser.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class ControllerUser extends Controller
 {
-    //
+    public function index(){
+        return view('user.index');
+    }
+
+    public function create(){
+        return view('user.create');
+    }
+
+    public function edit(){
+        return view('user.edit');
+    }
 }
---
+++ resources/views/user/create.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.layout-admin')
+
+@section('title', 'Data User')
+
+@section('page_title', 'Data User')
+
+@section('content')
+    Ini halaman tambah user
+@endsection
